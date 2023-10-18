0f07b2f9811f82033fe3f207d18126d86b478885
Gautam Singh <Gsingh3291@conestogac.on.ca>
2023-10-18 17:15:58 -0400

    Added calc functions.

--- Server_Side/Services/Analysis_Report_Services.cs
+++ Server_Side/Services/Analysis_Report_Services.cs
@@ -1,17 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Server_Side.Services
 {
-    /* This class for business logic and services of Analysis Report Module */
-    public class Analysis_Report_Services
-	{
-		public void Test_Analysis_Report_Services()
-		{
-            Console.WriteLine("Analysis_Report_Services Module Connected with Source file");
+    public class UserView
+    {
+        public string UserId { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+    public class PageView
+    {
+        public string SessionId { get; set; }
+        public string UserId { get; set; }
+        public string PageUrl { get; set; }
+        public string PageInfo { get; set; }
+        public string ProductId { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+
+    public class SaleTransaction
+    {
+        public string TransactionId { get; set; }
+        public string UserId { get; set; }
+        public decimal TransactionValue { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class Feedback
+    {
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+        public int StarRating { get; set; }
+    }
+
+    public class AnalyticsService
+    {
+        private List<UserView> Valid_User_Views_Table = new List<UserView>();
+        private List<PageView> Website_logs_table = new List<PageView>();
+        private List<SaleTransaction> SalesTransactionsTable = new List<SaleTransaction>();
+        private List<Feedback> FeedbackTable = new List<Feedback>();
+
+        public int GetUniqueVisitors(DateTime startDate, DateTime endDate)
+        {
+            return Valid_User_Views_Table
+                .Where(v => v.Timestamp >= startDate && v.Timestamp <= endDate)
+                .Select(v => v.UserId)
+                .Distinct()
+                .Count();
         }
+
+        public Dictionary<string, int> GetPageViews(DateTime startDate, DateTime endDate)
+        {
+            return Website_logs_table
+                .Where(v => v.DateTime >= startDate && v.DateTime <= endDate)
+                .GroupBy(v => v.PageUrl)
+                .ToDictionary(group => group.Key, group => group.Count());
+        }
+
+        public decimal GetTotalSales(DateTime startDate, DateTime endDate)
+        {
+            return SalesTransactionsTable
+                .Where(s => s.Date >= startDate && s.Date <= endDate)
+                .Sum(s => s.TransactionValue);
+        }
+
+        public decimal GetAverageOrderValue(DateTime startDate, DateTime endDate)
+        {
+            var salesInRange = SalesTransactionsTable
+                .Where(s => s.Date >= startDate && s.Date <= endDate)
+                .ToList();
+
+            var totalValue = salesInRange.Sum(s => s.TransactionValue);
+            var totalCount = salesInRange.Count;
+
+            return totalCount != 0 ? totalValue / totalCount : 0;
+        }
+
+        public Dictionary<string, int> GetFeedbackAnalysis()
+        {
+            return FeedbackTable
+                .GroupBy(f => f.ProductId)
+                .ToDictionary(group => group.Key, group => group.ToList().Sum(g => g.StarRating) / group.Count());
+        }
+
+        public decimal GetConversionRate(string productId, DateTime startDate, DateTime endDate)
+        {
+            var productPageViews = Website_logs_table
+                .Where(v => v.DateTime >= startDate && v.DateTime <= endDate && v.ProductId == productId)
+                .Count();
+
+            var productSales = SalesTransactionsTable
+                .Where(s => s.Date >= startDate && s.Date <= endDate && s.TransactionValue == decimal.Parse(productId))
+                .Count();
+
+            return productPageViews != 0 ? (decimal)productSales / productPageViews : 0;
+        }
+
     }
 }
