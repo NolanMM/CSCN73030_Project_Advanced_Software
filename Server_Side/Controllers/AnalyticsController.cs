cb1699e9d2a1a3543da3254e2aa23ed31a31689e
Knappkins <lknapp9090@Gmail.com>
2023-10-21 21:43:39 -0400

    Created the code that runs the html websites using asp.net 6.0. Also created endpoints for fetch requests.
    Co-authored-by: Gleb  <gignatov6034@users.noreply.github.com>

---
+++ Server_Side/Controllers/AnalyticsController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server_Side.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        //[Route("")] // This means it maps to /analytics
+        //[Route("index")] // This means it maps to /analytics/index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/api/salesData")]
+        public IActionResult GetSalesData()
+        {
+            var data = new
+            {
+                salesTotal = 1000,
+                viewTotal = 5000,
+                lifetimeSales = 50000,
+                averageSatisfaction = 4.5
+            };
+
+            // Set the Content-Type header to "application/json"
+            Response.ContentType = "application/json";
+
+            // Return the data as a JSON response
+            return Json(data);
+        }
+
+        [HttpGet("/api/productListData")]
+        public IActionResult GetProductListData()
+        {
+            var data = new
+            {
+                salesTotal = 1000,
+                viewTotal = 5000,
+                lifetimeSales = 50000,
+                averageSatisfaction = 4.5
+            };
+
+            // Set the Content-Type header to "application/json"
+            Response.ContentType = "application/json";
+
+            // Return the data as a JSON response
+            return Json(data);
+        }
+    }
+}
---
+++ Server_Side/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// This is the part that used to be in ConfigureServices
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// This is the part that used to be in the Configure method in Startup.cs
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Analytics}/{action=Index}/{id?}");
+});
+
+app.Run();
