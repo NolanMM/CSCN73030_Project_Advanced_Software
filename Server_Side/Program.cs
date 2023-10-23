7ea2bc95abd8e0f89a72dadd1ea522b7d9bd12f6
gignatov6034 <gignatov6034@conestogac.on.ca>
2023-10-22 22:55:54 -0400

    Added rate limiter
    Co-authored-by: Liam Knapp <Lknapp1110@conestogac.on.ca>
    Co-authored-by: Minh Nguyen <minhlenguyen7@gmail.com>

--- Server_Side/Program.cs
+++ Server_Side/Program.cs
@@ -1,12 +1,57 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AspNetCoreRateLimit;
+
+// Done:
+// ddos protection
+
+// Now:
+// dynamic variables
+// dynamic ip?
+// dynamic port?
+
+// Later:
+// Encrypted bytes (server to server)
+// certificate
+// sql injection
+// ssrf protection
+// assured authorization
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Define the port here
+
 
 // This is the part that used to be in ConfigureServices
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+
+// Add the rate limiting services
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(options =>
+{
+    options.EnableEndpointRateLimiting = true;
+    options.HttpStatusCode = 429;
+    options.RealIpHeader = "X-Real-IP";
+    options.ClientIdHeader = "X-ClientID";
+    options.GeneralRules = new List<RateLimitRule>
+    {
+        new RateLimitRule
+        {
+            Endpoint = "*", //"*" is used to target any HTTP verb
+            Limit = 25,  // Adjust the limit as needed
+            Period = "30s" // Adjust the period as needed (e.g., 20s for 20 seconds)
+        }
+    };
+});
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+builder.Services.AddInMemoryRateLimiting();
 
 var app = builder.Build();
 
@@ -15,6 +60,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+
+
+app.UseIpRateLimiting(); // Add the rate limiting middleware
 
 app.UseHttpsRedirection();  //more info about pages
 app.UseStaticFiles();       //allows to use wwwroot
